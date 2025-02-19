3ccb3bbb065dbe1c2e94088f2477e6bab42eb88a
Valentin Laptop <valentin.beaumelle@gmail.com>
2025-02-19 15:05:05 +0100

    Testing productGain

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import { Product, World } from './graphql';
+import { Product, RatioType, World } from './graphql';
 import { origworld } from './origworld';
 import * as path from 'path';
 
@@ -35,12 +35,17 @@ export class AppService {
     );
   }
 
-  updateScore(world: World){
+  updateScore(world: World): World{
+
     const now = Date.now()
     const timelaps = now - world.lastupdate;
     let gain = 0;
+    let pg;
     world.products.forEach(p => {
-      gain += this.productGain(p, timelaps);
+      pg = this.productGain(p, timelaps);
+      gain += pg.gain;
+      p.timeleft = pg.timeleft;
+      this.updateProduct(world, p);
     });
     world.angelupgrades.forEach(a => {
       if (a.unlocked) gain *= a.ratio;
@@ -50,31 +55,102 @@ export class AppService {
     world.lastupdate = now;
     world.money += gain;
     world.score += gain;
+
+    return world;
   }
 
-  private productGain(product: Product, timelaps: number): number{
+  private productGain(product, timelaps: number){
     if (product.managerUnlocked){
       if ((timelaps-product.timeleft) < 0){
-        product.timeleft = product.timeleft - timelaps;
-        return 0;
+        return {gain: 0, timeleft: product.timeleft - timelaps, qt: 0};
       }
       else {
-        product.timeleft = (timelaps-product.timeleft) % product.vitesse
-        return (((timelaps-product.timeleft) / product.vitesse) | 0) * product.revenu;
+        return {gain: ((((timelaps-product.timeleft) / product.vitesse) | 0) + 1) * product.revenu, timeleft: product.vitesse-((timelaps-product.timeleft) % product.vitesse), qt: ((((timelaps-product.timeleft) / product.vitesse) | 0) + 1)};
       }
     }
     else {
       if (product.timeleft === 0){
-        return 0;
+        return {gain: 0, timeleft: 0, qt: 0};
       }
       else if (timelaps > product.timeleft) {
-        product.timeleft = 0
-        return product.revenu;
+        return {gain: product.revenu, timeleft: 0, qt: 1};
       }
       else {
-        product.timeleft -= timelaps
+        return {gain: 0, timeleft: product.timeleft - timelaps, qt: 0};
       }
     }
-    return 0;
+  }
+
+  updateUnlocks(world: World, idProduct: number){
+    this.updateProduct(world, this.updateProductUnlock(this.findProduct(world, idProduct)));
+    let mini_seuil = Number.POSITIVE_INFINITY;
+    world.products.forEach(product => {
+      mini_seuil = Math.min(mini_seuil, product.quantite);
+    });
+
+    world.upgrades.forEach(palier => {
+      if (palier.unlocked === false && palier.seuil < mini_seuil){
+        palier.unlocked = true;
+        if (palier.typeratio = RatioType.gain){
+          world.products.map((p) => {
+            p.revenu *= palier.ratio;
+          })
+        }
+        else if (palier.typeratio = RatioType.vitesse){
+          world.products.map((p) => {
+            p.vitesse /= palier.ratio;
+          })
+        }
+      }
+    });
+
+    // TODO: Sauvegarder world
+  }
+
+  private updateProductUnlock(product: Product): Product{
+    product.paliers.forEach(palier => {
+      if (palier.unlocked === false && palier.seuil < product.quantite){
+        palier.unlocked = true;
+        if (palier.typeratio = RatioType.gain) product.revenu *= palier.ratio;
+        else if (palier.typeratio = RatioType.vitesse) product.vitesse /= palier.ratio;
+      }
+    });
+    return product;
+  }
+
+
+
+  findProduct(world: World, id: number){
+    return world.products.find((product) => product.id === id);
+  }
+
+  findManager(world: World, name: string){
+    return world.managers.find((manager => manager.name === name))
+  }
+
+  updateProduct(world: World, product: Product){
+    world.products.map((p) => {
+      if (p.id === product.id){
+        p = product;
+      }
+      else {
+        p = p;
+      }
+    })
+    return world
+  }
+  
+  updateManager(world: World, manager){
+    world.managers.map((m) => {
+      if (m.name === manager.name){
+        m = manager;
+      }
+      else {
+        m = m;
+      }
+    })
+    return world
   }
 }
+
+
--- backend/src/resolver.ts
+++ backend/src/resolver.ts
@@ -9,8 +9,9 @@ export class GraphQlResolver {
   @Query()
   async getWorld(@Args('user') user: string) {
     const world = this.service.readUserWorld(user);
-    this.service.updateScore(world);
-    return world;
+    const updated_world = this.service.updateScore(world);
+    this.service.saveWorld(user, updated_world)
+    return updated_world;
   }
 
   @Mutation()
@@ -32,7 +33,7 @@ export class GraphQlResolver {
         (1 - product.croissance));
     product.cout *= (1 + product.croissance) ** quantite;
 
-    world = this.updateProduct(world, product)
+    world = this.service.updateProduct(world, product)
     this.service.saveWorld(user, world);
     return product;
   }
@@ -43,14 +44,14 @@ export class GraphQlResolver {
     @Args('id') id: number,
   ) {
     let world = await this.getWorld(user);
-    let product = this.findProduct(world, id);
+    let product = this.service.findProduct(world, id);
     if (product == undefined)
       throw new Error(`Le produit avec l'id ${id} n'existe pas`);
 
     // Calcul du timeleft
     product.timeleft = product.vitesse;
 
-    world = this.updateProduct(world, product)
+    world = this.service.updateProduct(world, product)
     this.service.saveWorld(user, world);
     return product;
   }
@@ -61,52 +62,20 @@ export class GraphQlResolver {
     @Args('name') name: string,
   ) {
     let world = await this.getWorld(user);
-    let manager = this.findManager(world, name);
+    let manager = this.service.findManager(world, name);
     if (manager == undefined)
       throw new Error(`Le manager avec le nom ${name} n'existe pas`);
 
-    let product = this.findProduct(world, manager.idcible);
+    let product = this.service.findProduct(world, manager.idcible);
     
 
     // Update du manager
     product.managerUnlocked = true
     manager.unlocked = true
 
-    world = this.updateProduct(world, product)
-    world = this.updateManager(world, manager)
+    world = this.service.updateProduct(world, product)
+    world = this.service.updateManager(world, manager)
     this.service.saveWorld(user, world);
     return manager;
-  }
-
-
-  private findProduct(world: World, id: number){
-    return world.products.find((product) => product.id === id);
-  }
-
-  private findManager(world: World, name: string){
-    return world.managers.find((manager => manager.name === name))
-  }
-
-  private updateProduct(world: World, product: Product){
-    world.products.map((p) => {
-      if (p.id === product.id){
-        p = product;
-      }
-      else {
-        p = p;
-      }
-    })
-    return world
-  }
-  private updateManager(world: World, manager){
-    world.managers.map((m) => {
-      if (m.name === manager.name){
-        m = manager;
-      }
-      else {
-        m = m;
-      }
-    })
-    return world
   }
 }
